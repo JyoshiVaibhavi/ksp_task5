0f5b9b357dd808b62b6dd1735f7284ae59dfba30
Jyoshi Vaibhavi <147025036+JyoshiVaibhavi@users.noreply.github.com>
2023-10-11 18:10:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+// variables
+let var1 = 4;
+let var2 = 5;
+
+console.log(var1 + var2);
+console.log(var2);
+
+// camelcase:- We can update or change the camelcase variables. 
+
+let nameOfThePerson = "Vaibhavi";
+let dateOfBirth = 22;
+let ageOfThePerson = 18;
+
+console.log(nameOfThePerson);
+console.log(ageOfThePerson);
+console.log(dateOfBirth);
+
+
+// Constant:-We can't update or change the constant variables.
+
+const num1 = 24;
+// num1=22-This will produce a type error.
+console.log(num1);
+
+//Operators
+// Arithmetic operator:- +, -, *, /, **, %
+
+let number1 = 10;
+let number2 = 20;
+
+console.log(number1 + number2);
+console.log(number1 - number2);
+console.log(number1 * number2);
+console.log(number1 / number2);
+console.log(number1 ** number2);
+
+console.log(number1 % number2);
+
+// Assignment operator:- =, +=, -=, *=, /=, %=, **=
+
+let event1 = 100;
+let event2 = 100;
+let event3 = 150;
+
+console.log(event1);
+console.log(event1 = event2);
+console.log(event1 /= event3);
+console.log(event2 += event3);
+console.log(event2 -= event3);
+console.log(event2 *= event3);
+console.log(event2 %= event3);
+console.log(event2 **= event3);
+
+//Comparision operator:- ==, ===, !=, <, >, <=, >=
+// ==: It will check the value.
+// ===:It will check value as well as data type.
+
+let banana = 12;
+let mango = 6;
+let cherries = 12;
+
+console.log(banana == cherries);
+console.log(banana === mango);
+console.log(banana != mango);
+console.log(banana < mango);
+console.log(cherries > mango);
+console.log(mango <= cherries);
+console.log(cherries >= banana);
+
+
+
+
+
+
+
+
+
